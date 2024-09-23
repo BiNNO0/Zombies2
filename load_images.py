6ec95d7994ffcf8a25cad361f42adad88b360413
BiNN0 <angeillescasuriel11@gmail.com>
2024-09-23 16:51:43 -0600

    initcommit

---
+++ load_images.py
@@ -0,0 +1,15 @@
+from pygame import image
+
+bg = image.load(r"images\bg.png")
+
+
+idle = image.load(r"images\player_1.png")
+
+pl_move = [image.load(r"images\player_1.png"),
+           image.load(r"images\player_2.png"),
+           image.load(r"images\player_3.png")]
+
+pl_move = False
+
+zombie = image.load(r"images\zombie.png")
+                                                
